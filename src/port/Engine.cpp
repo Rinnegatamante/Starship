4347969a3cbe35c1c84b349dd4c80c595089e312
Rinnegatamante <rinnegatamante@gmail.com>
2026-03-29 21:24:53 +0200

    Default to 30 FPS.

--- src/port/Engine.cpp
+++ src/port/Engine.cpp
@@ -349,7 +349,7 @@ void GameEngine::ProcessGfxCommands(Gfx* commands) {
     wnd->EnableSRGBMode();
     wnd->SetRendererUCode(UcodeHandlers::ucode_f3dex);
     std::vector<std::unordered_map<Mtx*, MtxF>> mtx_replacements;
-    int target_fps = CVarGetInteger("gInterpolationFPS", 60);
+    int target_fps = CVarGetInteger("gInterpolationFPS", 30);
     static int last_fps;
     static int last_update_rate;
     static int time;
