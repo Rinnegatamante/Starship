c030dbc28e0785dbe935bcf9f8523e5ce5cb498a
Rinnegatamante <rinnegatamante@gmail.com>
2026-04-19 17:14:01 +0200

    Don't run frame interpolation when targeting 30 FPS.

--- src/port/interpolation/FrameInterpolation.cpp
+++ src/port/interpolation/FrameInterpolation.cpp
@@ -430,7 +430,7 @@ void FrameInterpolation_StartRecord(void) {
         is_recording = false;
         return;
     }
-    if (GameEngine::GetInterpolationFPS() != 20) {
+    if (GameEngine::GetInterpolationFPS() != 30) {
         is_recording = true;
     }
 }
